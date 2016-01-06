1ed0c69bd654e3a542cc38420cf829c44ad2f528
Berkay <berkay@macbook-pro.local>
2016-01-06 13:50:33 -0800

    Initial Commit

---
+++ UkayAppTests/UkayAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  UkayAppTests.swift
+//  UkayAppTests
+//
+//  Created by Berkay Surmeli on 1/6/16.
+//  Copyright © 2016 Berkay Surmeli. All rights reserved.
+//
+
+import XCTest
+@testable import UkayApp
+
+class UkayAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
